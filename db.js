ce753318990bf3b649004683b875104344253aba
zorawar1090 <zorawarsingh9988@gmail.com>
2019-09-19 15:19:05 +0200

    make db immutable

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const databaseUrl = 'postgres://postgres:password@localhost:5432/postgres'
 
 const db = new Sequelize(databaseUrl)
 
-db.sync({ force: true })
+db.sync({ force: false })
   .then(() => console.log('Database connected'))
   .catch(console.error)
 
